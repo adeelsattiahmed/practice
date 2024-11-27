8627bb85b62018fea6016c7890792898c2b227cb
adeelsattiahmed <147237219+adeelsattiahmed@users.noreply.github.com>
2024-11-27 08:41:58 +0500

    Add files via upload

---
+++ Machine Learning/matplotlib/practice on IDE/a1.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import  train_test_split
+import seaborn as sns
+
+df =pd.read_csv("Ecommerce Purchases")
+
